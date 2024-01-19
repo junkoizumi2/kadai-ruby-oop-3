38fd2336b8a79707f94898336eaede9c56ce7199
Junko.Izumi <mugiosanmugio@gmail.com>
2024-01-19 00:41:59 +0000

    update sample.txt

--- main.rb
+++ main.rb
@@ -1,6 +1,5 @@
 require './human'
 
-class Main 
   tanaka = Human.new('田中 太郎',25,'電車')
   suzuki = Human.new('鈴木 次郎',30,'野球')
   sato   = Human.new('佐藤 花子',20,'映画')
@@ -15,5 +14,3 @@ class Main
   sato.say
   sato.think
   
-  
-end
